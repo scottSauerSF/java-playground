dac5b7d532fa0170fff2d8b5638127979d53a515
Scott Sauer <scott.sauer.lvlb@statefarm.com>
2021-11-21 15:38:57 -0600

    initial commit

---
+++ src/main/java/com/galvanize/SomeProgram.java
@@ -0,0 +1,14 @@
+package com.galvanize;
+
+public class SomeProgram {
+    public static void main(String[] args) {
+        String firstArgument = args[0];   // arg1 name
+        String secondArgument = args[1];  // arg2 email
+
+        System.out.println();
+    }
+
+    class BankAccount{
+
+    }
+}
